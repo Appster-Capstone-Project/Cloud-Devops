381646dac3997fea4b6d4ca2f770e112112f6425
Arun Sebastian <91450545+arunseb@users.noreply.github.com>
2025-06-27 16:56:08 -0400

    updated infra code

--- terraform-docker-app/outputs.tf
+++ terraform-docker-app/outputs.tf
@@ -1,3 +1,19 @@
-variable "location" {
-  default = "East US"
+# Output the name of the created Azure Resource Group.
+output "resource_group_name" {
+  description = "The name of the Azure Resource Group created."
+  value       = azurerm_resource_group.rg.name
+}
+
+# Output the public IP address of the Docker VM.
+# This is crucial for accessing your VM (e.g., via SSH or a web server running on it).
+output "vm_public_ip" {
+  description = "The public IP address of the Docker VM."
+  value       = azurerm_public_ip.public_ip.ip_address
+}
+
+# Output the Azure region where the VM is deployed.
+# This refers to the 'location' input variable used in main.tf.
+output "vm_location" {
+  description = "The Azure region where the VM is deployed."
+  value       = var.location
 }
--- terraform-docker-app/variables.tf
+++ terraform-docker-app/variables.tf
@@ -1,3 +1,28 @@
+# Input variable for the Azure region where resources will be deployed.
+# Using a variable makes your Terraform configuration reusable for different regions.
 variable "location" {
-  default = "East US"
+  description = "The Azure region where resources will be deployed."
+  type        = string
+  default     = "East US" # Default value for the location, can be overridden.
 }
+
+# Example: You might want to define other variables for flexibility.
+# variable "vm_size" {
+#   description = "Size of the virtual machine."
+#   type        = string
+#   default     = "Standard_B1s"
+# }
+
+# variable "admin_username" {
+#   description = "Admin username for the VM."
+#   type        = string
+#   default     = "azureuser"
+# }
+
+# IMPORTANT: For production, do NOT hardcode passwords. Use a secure method like Azure Key Vault.
+# Mark sensitive variables to prevent them from being shown in logs.
+# variable "admin_password" {
+#   description = "Admin password for the VM. Highly recommend using Key Vault for production!"
+#   type        = string
+#   sensitive   = true # This prevents the value from being printed in Terraform logs.
+# }
