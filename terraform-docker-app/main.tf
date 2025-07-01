567f30d784ed881f79c654ee47c5e21903fbd048
Arun Sebastian <91450545+arunseb@users.noreply.github.com>
2025-07-01 22:59:24 +0000

    Rename network security group resource for clarity

--- terraform-docker-app/main.tf
+++ terraform-docker-app/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_resource_group" "rg" {
   location = var.location   # Uses the 'location' variable defined in variables.tf
 }
 
-resource "azurerm_network_security_group" "rg" {
+resource "azurerm_network_security_group" "security_group" {
   name                = "acceptanceTestSecurityGroup1"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
