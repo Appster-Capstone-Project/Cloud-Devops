f96a44f71cd3f41c9d3647eb59034b6a35c73638
ALBIN BABU VARGHESE <albinbabuvarghese20@gmail.com>
2025-08-08 20:49:28 -0400

    Use Docker Compose for backend

--- terraform-docker-app/docker-startup.sh
+++ terraform-docker-app/docker-startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
-apt-get install -y docker.io git openssl
+apt-get install -y docker.io docker-compose-plugin git openssl
 usermod -aG docker azureuser
 
 # Generate a self-signed certificate for HTTPS
@@ -21,12 +21,9 @@ if [[ "$HOSTNAME" == *"frontend"* ]]; then
     -e SSL_KEY=/etc/ssl/docker/selfsigned.key \
     yourdockerhubuser/frontend-image:latest
 elif [[ "$HOSTNAME" == *"backend"* ]]; then
-  # Pull and run the backend container exposing HTTPS
-  docker pull yourdockerhubuser/backend-image:latest
-  docker run -d --name backend -p 5000:5000 -p 443:5000 \
-    -v /etc/ssl/docker:/etc/ssl/docker \
-    -e SSL_CERT=/etc/ssl/docker/selfsigned.crt \
-    -e SSL_KEY=/etc/ssl/docker/selfsigned.key \
-    yourdockerhubuser/backend-image:latest
+  # Clone and run the backend application using Docker Compose
+  git clone https://github.com/Appster-Capstone-Project/Capstone-Project-Backend.git /opt/backend
+  cd /opt/backend || exit
+  docker compose up -d
 fi
 
